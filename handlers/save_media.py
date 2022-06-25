b87547ebbb42e85a54f67d554c808f5993785e5b
telegrambotes <108186703+telegrambotes@users.noreply.github.com>
2022-06-25 23:17:30 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -86,7 +86,7 @@ async def SaveMediaInChannel(bot: Client, editable: Message, message: Message):
         await forwarded_msg.reply_text(
             f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
             parse_mode="Markdown", disable_web_page_preview=True)
-        share_link = f"`https://t.me/{Config.BOT_USERNAME}?start=PredatorHackerzZ_{str_to_b64(file_er_id)}`"
+        share_link = f"https://t.me/{Config.BOT_USERNAME}?start=PredatorHackerzZ_{str_to_b64(file_er_id)}"
         await editable.edit(
             f"**Your Media Stored in My Database!**\n\nHere is the Permanent Shareable Link Of Your Files:\n {share_link} \n\n"
             f"Just Click On the Link to Reterive the Files!",
