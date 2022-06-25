cf59fd09cd2589f33ed992bb79d3a4a9f14155ad
telegrambotes <108186703+telegrambotes@users.noreply.github.com>
2022-06-26 00:04:41 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -7,4 +7,29 @@ from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
 
+async def ReplyForward(message: Message, file_id: int):
+   
+            disable_web_page_preview=True, quote=True)
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        await ReplyForward(message, file_id)
+
+
+async def MediaForward(bot: Client, user_id: int, file_id: int):
+    try:
+        if Config.FORWARD_AS_COPY is True:
+            return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+                                          message_id=file_id)
+        elif Config.FORWARD_AS_COPY is False:
+            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+                                              message_ids=file_id)
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return MediaForward(bot, user_id, file_id)
+
+
+async def SendMediaAndReply(bot: Client, user_id: int, file_id: int):
+    sent_message = await MediaForward(bot, user_id, file_id)
+    await ReplyForward(message=sent_message, file_id=file_id)
+    await asyncio.sleep(2)
 
